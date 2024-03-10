abc5280b5e3b120279708c715a50954e71819a7e
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2024-03-10 13:35:43 +0600

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
         $products = Product::all();
         return view('products', compact('products'));
     }
-  
+
     /**
      * Write code on Method
      *
@@ -28,7 +28,7 @@ class ProductController extends Controller
     {
         return view('cart');
     }
-  
+
     /**
      * Write code on Method
      *
@@ -37,10 +37,10 @@ class ProductController extends Controller
     public function addToCart($id)
     {
         $product = Product::findOrFail($id);
-          
+
         $cart = session()->get('cart', []);
-  
-        if(isset($cart[$id])) {
+
+        if (isset($cart[$id])) {
             $cart[$id]['quantity']++;
         } else {
             $cart[$id] = [
@@ -50,11 +50,11 @@ class ProductController extends Controller
                 "image" => $product->image
             ];
         }
-          
+
         session()->put('cart', $cart);
         return redirect()->back()->with('success', 'Product added to cart successfully!');
     }
-  
+
     /**
      * Write code on Method
      *
@@ -62,7 +62,7 @@ class ProductController extends Controller
      */
     public function update(Request $request)
     {
-        if($request->id && $request->quantity){
+        if ($request->id && $request->quantity) {
             $cart = session()->get('cart');
             $cart[$request->id]["quantity"] = $request->quantity;
             session()->put('cart', $cart);
@@ -76,60 +76,64 @@ class ProductController extends Controller
      */
     public function remove(Request $request)
     {
-        if($request->id) {
+        if ($request->id) {
             $cart = session()->get('cart');
-            if(isset($cart[$request->id])) {
+            if (isset($cart[$request->id])) {
                 unset($cart[$request->id]);
                 session()->put('cart', $cart);
             }
             session()->flash('success', 'Product removed successfully');
         }
     }
-    public function checkout(){
+    public function checkout()
+    {
         return view('checkout');
     }
     public function order(Request $request): RedirectResponse
-    {  
-        if(session('cart')){
-            $carts = session('cart'); 
+    {
+        if (session('cart')) {
+            $carts = session('cart');
             $total = 0;
             $customerID = 1;
-            foreach($carts as $id=>$details){
+            foreach ($carts as $id => $details) {
                 echo $id . '<br>';
                 $total += $details['price'] * $details['quantity'];
             }
         }
-       $order_data = [
+        $order_data = [
             'totalamount' => $total,
             'customer_id' => $customerID,
             'coupon_discount' => 0,
             'payment_method' => 'stripe'
-       ];
-       $order_id = Order::insertGetId($order_data);
-       //echo $order_id;
-        foreach($carts as $id=>$details){
-        $order_details_data = [
-            'order_id' => $order_id,
-            'product_id' => $id,
-            'product_qty' => $details['quantity'],
-            'product_price' => $details['price'],
-            'product_subtotal' => $details['quantity'] * $details['price'],
-            'product_discount' => 0,     
-       ];
-       OrderDetails::insert($order_details_data);
-    }   
-       //dd($carts);     
-       
-       Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-      
-        Stripe\Charge::create ([
-                "amount" => $total,
-                "currency" => "usd",
-                "source" => $request->stripeToken,
-                "description" => "Test payment from TCLK." 
+        ];
+        $order_id = Order::insertGetId($order_data);
+        //echo $order_id;
+        foreach ($carts as $id => $details) {
+            $order_details_data = [
+                'order_id' => $order_id,
+                'product_id' => $id,
+                'product_qty' => $details['quantity'],
+                'product_price' => $details['price'],
+                'product_subtotal' => $details['quantity'] * $details['price'],
+                'product_discount' => 0,
+            ];
+            OrderDetails::insert($order_details_data);
+
+            //    session_destroy(session('cart'));
+            session()->forget('cart');
+        }
+        //dd($carts);     
+
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        Stripe\Charge::create([
+            "amount" => $total,
+            "currency" => "usd",
+            "source" => $request->stripeToken,
+            "description" => "Test payment from TCLK."
         ]);
-                
+
         return redirect('products')
-                ->with('success', 'Order successful!');
+            ->with('success', 'Order successful!');
     }
 }
